fee53cf4f230f8d16da4023234a4564e152e92e5
XxSojasxX <sojasvaca@gmail.com>
2023-11-22 20:18:51 -0500

    Dto's

--- src/album/dto/create-album.dto.ts
+++ src/album/dto/create-album.dto.ts
@@ -1,1 +1,25 @@
-export class CreateAlbumDto {}
+import { IsDateString, IsNotEmpty, IsNumberString, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreateAlbumDto {
+    @IsString()
+    @IsNotEmpty()
+    nombre: string;
+
+    @IsDateString()
+    @IsNotEmpty()
+    lanzamiento: Date;
+
+    @IsNumberString()
+    @IsNotEmpty()
+    @MinLength(1)
+    pistas: number;
+
+    @IsString()
+    @IsNotEmpty()
+    autor: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    disquera: string
+}

14a51b47e3609fc342c8231f03ac3392241a568a
XxSojasxX <sojasvaca@gmail.com>
2023-11-22 19:59:02 -0500

    Conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumModule } from './album/album.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AlbumModule],
+  imports: [
+    TypeOrmModule,
+    AlbumModule,
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host:'localhost',
+      port: 5432,
+      username:'postgres',
+      password: 'root',
+      database:'disquera',
+      autoLoadEntities: true,
+      synchronize: true,
+      dropSchema: true,
+
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
